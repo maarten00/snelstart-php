fce8a6c57792428d8b69f3ae231ba82869a1729a
Maarten Kuiper <maarten@uxico.dev>
2025-08-13 21:35:31 +0200

    Add DocumentConnector to retrieve a document including its contents

---
+++ src/Connector/V2/DocumentConnector.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author  IntoWebDevelopment <info@intowebdevelopment.nl>
+ * @project SnelstartApiPHP
+ * @deprecated
+ */
+
+namespace SnelstartPHP\Connector\V2;
+
+use Ramsey\Uuid\UuidInterface;
+use SnelstartPHP\Connector\BaseConnector;
+use SnelstartPHP\Exception\SnelstartResourceNotFoundException;
+use SnelstartPHP\Mapper\V2 as Mapper;
+use SnelstartPHP\Model\V2 as Model;
+use SnelstartPHP\Request\V2 as Request;
+
+final class DocumentConnector extends BaseConnector
+{
+    public function find(UuidInterface $uuid): ?Model\Document
+    {
+        $documentRequest = new Request\DocumentRequest();
+        $documentMapper = new Mapper\DocumentMapper();
+
+        try {
+            return $documentMapper->find($this->connection->doRequest($documentRequest->find($uuid)));
+        } catch (SnelstartResourceNotFoundException $e) {
+            return null;
+        }
+    }
+}
